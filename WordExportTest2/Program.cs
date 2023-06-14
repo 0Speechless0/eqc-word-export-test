9f0b2424b5cc97531773d2ab39c9dc8caf606c33
Coffee <kbb37038106@gmail.com>
2023-06-14 19:40:59 +0800

    添加项目文件。

---
+++ WordExportTest2/Program.cs
@@ -0,0 +1,29 @@
+﻿using EQC.ProposalV2;
+using EQC.ProposalV2.EDMXModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordExportTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            // get directory
+            var solutionPath = @"..\..\";
+
+
+            var ExportInstance = new ExportEngRiskInstance(1);
+            ExportInstance.Export(
+                Path.Combine(solutionPath, @".\output.docx"),
+               Path.Combine(solutionPath, @".\template\Test2.docx"),
+               Path.Combine(solutionPath, @".\attachment")
+            );
+        }
+    }
+}
